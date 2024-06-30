77f61cd589e0d0129ed6f2f7dcaa191839e6c3fb
RudeDude29 <godaraashish029@gmail.com>
2024-06-30 16:46:37 +0530

    New Commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors')
-require("../config/db.js");
-const {dataRoutes} = require('../routes/dataRoutes.js');
+require("./config/db.js");
+const {dataRoutes} = require('./routes/dataRoutes.js');
 const app = express();
 
-app.use(cors());
 app.use("/", express.static(__dirname + "/public"));
+app.use(cors());
 app.use(express.json());
 app.use('/api',dataRoutes);
 
